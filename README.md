ddc0422b956cf3b66b1f0690d4fa2b41a7e35d85
Matthew Harrison <Matthew.Harrison@noaa.gov>
2023-07-18 16:51:12 -0400

    Initial commit of preprocessing scripts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MOM6_Pacific
-Regional Pacific domain configuration for MOM6 
+Regional Pacific domain configuration for MOM6
+
+
+
+Requires: xarray, matplotlib numpy and netCDF4
---
+++ preprocessing/input_data/om4_025/wget.bash
@@ -0,0 +1,3 @@
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/om4_025/basin_codes.nc
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/om4_025/topog.nc
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/om4_025/ocean_hgrid.nc
